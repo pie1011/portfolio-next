34bcde6ea0c15cd50ea754b18688033e78326d27
Katie <katieharshman1011@gmail.com>
2023-05-27 05:07:16 +0000

    minor updates

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,16 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+
+import Navigation from '../src/Navigation';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
+    <Container>
+      <Navigation />
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Hi, I'm Katie.
